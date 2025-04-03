3211de56fd75a572f52efb24f6084ba4fa23d440
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-03 09:13:44 +0000

    [add]bootstrap導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
+// 独自のcssを読み込ませるときはここ変更
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
