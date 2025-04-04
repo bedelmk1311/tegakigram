61bdf3b7c11a0de17d77a06b386c6021e8af024b
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 10:21:41 +0000

    [add]model post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,17 @@
 #  label_id   :integer          not null
 #
 class Post < ApplicationRecord
+  #active_storage宣言
+  has_one_attached :post_image
+
+  #アソシエーション
+  belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  #バリデーション
+  validates :boby, presence: true
+  validates :image, presence: true
+  validates :label_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #active_storage宣言
+  has_one_attached :profile_image
+
+  has_many :posts
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  
+  # 自分がフォローされる（被フォロー）側の関係性
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
+  # 自分がフォローする（与フォロー）側の関係性
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+
+  #バリデーション
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
 end
