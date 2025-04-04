3717c80289a87ac9176153238a9bae9eafe27234
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 07:02:56 +0000

    [finish]routing public

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
-# == Route Map
-# ターミナルに入れるとルーティング情報を書き出す
-# bundle exec annotate --routes
+### == Route Map
+### ↓ターミナルに入れるとルーティング情報を書き出す
+### bundle exec annotate --routes
 
 Rails.application.routes.draw do
   # 管理者用 ログイン機能のみルーティングに登録
@@ -11,12 +11,17 @@ Rails.application.routes.draw do
 
   #
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # namespaceでadminをまとめる
   namespace :admin do
     root to: "homes#top"
-    resources :users, only: [:index, :show, :destroy]
+    resources :users, only: [:show, :index, :destroy] # こっちのdestroyは投稿を消したい
+
+    get "comfirm_post", to: "comfirm_post#comfirm" #仮
+    get "comfirm_comment", to: "comfirm_post#comfirm" #仮
+    get "index_comment", to: " # " #仮
+    delete "URL", to: " # " #仮 コメントを消したい
+
     get "search", to: "searches#search"
     # 検索機能にアクセスするためのルーティング
   end
@@ -24,24 +29,23 @@ Rails.application.routes.draw do
   #scope moduleでカスタマイズ
   scope module: :public do
     root to:"home#top"
-    resources :, only: [:index, :show]
-    get "home/about"=>"homes#about"
-  
+
+    #postと子(commentとfavorite)のルーティング
     resources :posts do
       resources :comments, only: [:create, :destroy]
       resource :favorites, only: [:create, :destroy]
     end
 
-
+    #userとrelationshipのルーティング
     resources :users, only: [:show,:edit,:update] do
-      patch "users/withdraw" => "users#withdraw"
       get "users/confirm" => "users#confirm"
-
-      resource :relationships, only: [:create, :destroy]
-      get "followings" => "relationships#followings", as: "followings"
-      get "followers" => "relationships#followers", as: "followers"
+      patch "users/withdraw" => "users#withdraw"
+        resource :relationships, only: [:create, :destroy]
+          get "followings" => "relationships#followings", as: "followings"
+          get "followers" => "relationships#followers", as: "followers"
     end
-    get 'search', to: 'searches#search'
+     #searshのルーティング
+    get "search", to: "searches#search"
 
 
 end
