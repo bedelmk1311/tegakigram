fa45f64c4a4ac996a6f352f604b49cfbabc20e72
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 09:24:06 +0000

    [add]controller/admin action

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,7 @@
 class Admin::SearchesController < ApplicationController
+
+  def search
+
+  end
+  
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,33 @@
 class Admin::UsersController < ApplicationController
+
+  def show
+    
+  end
+    
+  def index 
+    
+  end
+
+  def index_comments
+
+  end
+
+    
+  def comfirm_post
+    
+  end
+
+    
+  def comfirm_post
+    
+  end
+
+  def destroy
+
+  end
+
+  def destroy_comment
+
+  end
+
 end
