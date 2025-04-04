3c53cda898ecb1abbc4477cf9de0c610e0ca7730
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 06:28:17 +0000

    [finish]routing admin

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,15 @@ Rails.application.routes.draw do
   #
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # namespace :[グループ名] do
+  # [グループ]にまとめる場合
+  # end
+  
+  namespace :admin do
+    root to: "homes#top"
+    resources :users, only: [:index, :show, :destroy]
+    get "/search", to: "searches#search"
+    # 検索機能にアクセスするためのルーティング
+
 end
