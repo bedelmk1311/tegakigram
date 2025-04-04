44e475870c19aa2b301b12e9c059eeadb9565e08
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 09:52:18 +0000

    [add]table favorites

--- db/migrate/20250404093534_create_favorites.rb
+++ db/migrate/20250404093534_create_favorites.rb
@@ -2,9 +2,6 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
 
-      t.integer :follower_id
-      t.integer :followed_id
-      
       t.timestamps
     end
   end
