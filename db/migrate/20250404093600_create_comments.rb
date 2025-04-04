c68f07d6bdabc74ce13f72163a38ce42083eac48
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 10:06:03 +0000

    テーブル作成migrate前

--- db/migrate/20250404093600_create_comments.rb
+++ db/migrate/20250404093600_create_comments.rb
@@ -2,7 +2,10 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
 
-      t.text :message
+      t.text :message, null:false
+      t.integer :user_id
+      t.integer :post_id
+
       t.timestamps
     end
   end
