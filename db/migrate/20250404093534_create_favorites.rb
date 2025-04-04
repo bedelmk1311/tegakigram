180156d446d88b9338c05aeb7ab56f4b7eb48f61
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-04 09:48:11 +0000

    [add]table relationships

--- db/migrate/20250404093534_create_favorites.rb
+++ db/migrate/20250404093534_create_favorites.rb
@@ -2,6 +2,9 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
   def change
     create_table :favorites do |t|
 
+      t.integer :follower_id
+      t.integer :followed_id
+      
       t.timestamps
     end
   end
