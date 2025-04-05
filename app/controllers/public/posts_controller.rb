66b390ad5000fe18fcf18ab7b471294a17e11ca5
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-05 14:52:32 +0000

    [add]action posts/indexとshow

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -16,11 +16,19 @@ class Public::PostsController < ApplicationController
 
     
   def index
-    
+    @posts = Post.all
+    #@post = Post.new # 後でサイドバーから投稿する用
+
+    #@posts = PostImage.page(params[:page]) #メシテロのようにするならば
+    #1ページ分の決められた数のデータだけを、新しい順に取得
   end
 
     
   def show
+    #特定のidのPostImageモデルを格納させる
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    #コメントを投稿するためのインスタンス変数を定義する（空箱を用意する）
     
   end
 
