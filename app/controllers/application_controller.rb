fe54de71f469f4e2c712f302d613a8667f11dcb5
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-05 15:15:36 +0000

    [edit]action posts/index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
 class ApplicationController < ActionController::Base
+  #before_action :authenticate_user!, except: [:top]
+
+  #ログイン認証が成功していないと、許可以外の画面は表示できない仕様
+  #あとでコメントアウト戻す
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #deviseの機能が使われる前に、configure_permitted_parametersをする
+
   def after_sign_in_path_for(resource)
     posts_path #ログイン後の遷移先
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path #ログアウト後は最初に戻る
+  end
+
+  protected #呼び出された他のコントローラからも参照できる
+  #なんだっけ？いるっけ？
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  #deviseの機能が使われる前に、configure_permitted_parametersをする
+
   end
 end
