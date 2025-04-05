d55d7bf5f93f2fdaebc778329284751a5544d3ba
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-05 15:51:33 +0000

    [edit]action posts サイドバー

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   protected #呼び出された他のコントローラからも参照できる
   #なんだっけ？いるっけ？
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   #deviseの機能が使われる前に、configure_permitted_parametersをする
 
   end
