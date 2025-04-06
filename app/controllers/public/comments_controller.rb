2d3c01de0d4b229e0be1c514c72b24415db65ed1
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-06 06:52:00 +0000

    [add]view public/user/comment

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,14 +1,23 @@
 class Public::CommentsController < ApplicationController
 
   def index 
-
   end
 
   def create
-    
+    @post =Post.find(params[:post_id]) #修正:idだとcomment_idを代入してしまう
+    #@comment = Comment.new(comment_params) #修正
+    @comment = current_user.messages.new(comment_params)
+    post_id = @comment.post_id #
+    @comment.save
   end
     
   def destroy 
-    
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:message)
   end
 end
