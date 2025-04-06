57be7c3fb43b6306c6558cef939d0cac364aa070
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-06 07:15:42 +0000

    [add]view public/user/favorite

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,10 +1,16 @@
 class Public::FavoritesController < ApplicationController
   def create
-    
+    post = Post.find(param[:post_id]) #idではだめ
+    @favorite = current_user.favorites.new(post_id: post_id) #なんで複数形なんだっけ？
+    @favorite.save
+    render 'replace_btn'
+
   end
     
   def destroy 
-    
+    post = Post.find(params[:post_id])
+    @favorite = current_user.favorites.find_by(post_id: post.id)
+    @favorite.destroy
+    render 'replace_btn'
   end
-
 end
