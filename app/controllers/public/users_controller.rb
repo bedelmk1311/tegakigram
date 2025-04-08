b1cb745beb202fbead8e9faa5461e0c647c264ae
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-08 08:21:18 +0000

    [edit]user/confirm

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,6 +6,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+     #@user = current_user #どっちがいいんだろう
     @post = Post.new
     @posts = @user.posts
     @posts_all = Post.all
@@ -16,9 +17,11 @@ class Public::UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id]) #ensureの後で外す
+     #@user = current_user #どっちがいいんだろう
   end
 
   def update
+     #@user = current_user #どっちがいいんだろう
     @user = User.find(params[:id]) #ensureの後で外す
       if @user.update(user_params) #引数を指定しないとargumentエラー
         redirect_to user_path(@user), notice: "更新に成功しました"
@@ -30,13 +33,14 @@ class Public::UsersController < ApplicationController
   def confirm 
   end
 
-  #def withdraw #退会処理　後ほど
+  def withdraw #退会処理　後ほど
     #@user = User.find(params[:id])
+    #@user = User.find(user_params)
     #@user.update(is_user_status: false))
     #reset_session
     #flash[:notice] = "退会処理を実行しました。"
     #redirect_to root_path
-  #end
+  end
 
   private
 
