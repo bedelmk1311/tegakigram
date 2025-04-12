4ac5b71234c3157a5f6779fdd2c550b0b21eaa81
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-12 08:54:39 +0000

    ゲストログインのエラーを解消

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -34,6 +34,11 @@ class Public::SessionsController < Devise::SessionsController
     #root_path
   #end
 
+  def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to user_path(user), notice: "guestuserでログインしました。"
+  end
   #private #あとで
   #def block_if_admin_signed_in
     #if admin_signed_in?
