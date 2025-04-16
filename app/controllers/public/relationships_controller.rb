d45777407d448a8cc98e196266260eeacf98582b
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-16 13:53:51 +0000

    [add]flash

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,13 +4,14 @@ class Public::RelationshipsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow(user) #userモデルで定義
-    redirect_to request.referer #
+    redirect_to request.referer, notice: "フォローしました"
+    #直前に見ていたURL=同じページに戻る
   end
     
   def destroy 
     user = User.find(params[:user_id])
     current_user.unfollow(user)
-		redirect_to request.referer
+		redirect_to request.referer , notice: "フォローを外しました"
   end
 
     
