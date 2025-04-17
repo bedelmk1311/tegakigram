890f2db1f5efe5566022a06453c0c6e6adc6065c
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-17 12:31:28 +0000

    [add]view,controller

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,7 +1,5 @@
 class Admin::HomesController < ApplicationController
 
   def top
-
   end
-  
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,31 +3,23 @@ class Admin::UsersController < ApplicationController
   def show
     
   end
-    
+
   def index 
     
   end
 
-  def index_comments
-
-  end
-
-    
-  def comfirm_post
-    
-  end
-
-    
-  def comfirm_post
-    
-  end
+  # def index_comments
+  # end
+  # def comfirm_post
+  # end
+  # def comfirm_post
+  # end
 
   def destroy
 
   end
 
-  def destroy_comment
-
-  end
+  # def destroy_comment
+  # end
 
 end
