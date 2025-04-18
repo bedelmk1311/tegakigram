0a15d694eb9ca90fc06e20b64dff3b1c3ab40b48
MikaKanno <91572763+bedelmk1311@users.noreply.github.com>
2025-04-18 14:38:38 +0000

    [middle]テンプレ化 comment/index

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -8,17 +8,19 @@ class Public::CommentsController < ApplicationController
     @comment = current_user.comments.new(comment_params) #現在のuserのコメントを作成する（messageを受け取って）
     @comment.post_id = @post.id 
     if @comment.save
-      redirect_to post_path(@post), notice: "コメントの投稿に成功しました" #戻る
+      #redirect_to post_path(@post), notice: "コメントの投稿に成功しました" #戻る
+      back_redirect_by_notice("コメントの投稿に成功しました")
     else
       render 'error' 
     end
-    end
-    
+  end
+
   def destroy 
     @post =Post.find(params[:post_id]) 
     @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to post_path(@post) , notice: "コメントは削除されました" #戻る
+    #redirect_to post_path(@post) , notice: "コメント削除の削除に成功しました" #戻る
+    back_redirect_by_notice("コメント削除の削除に成功しました")
   end
 
   private
